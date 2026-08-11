41313da1261e077543071b3a43932558f3793b60
Kunal0108 <kunalgurnani0108@gmail.com>
2026-08-11 20:37:58 -0300

    Time: 2 ms (97.12%) | Memory: 43.8 MB (15.66%) - LeetSync

---
+++ 1095-two-city-scheduling/two-city-scheduling.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        int total = 0;
+
+        Arrays.sort(costs, (a, b) ->
+            (a[0] - a[1]) - (b[0] - b[1])
+        );
+
+        int n = costs.length / 2;
+
+        for (int i = 0; i < costs.length; i++) {
+            if (i < n) {
+                total += costs[i][0]; // City A
+            } else {
+                total += costs[i][1]; // City B
+            }
+        }
+
+        return total;
+    }
+}
