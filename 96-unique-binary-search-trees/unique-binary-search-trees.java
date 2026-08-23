2cbda7f410352deacff3600ec2b34b3d67a99c8b
Kunal0108 <kunalgurnani0108@gmail.com>
2026-08-23 14:34:02 -0300

    Time: 0 ms (100.00%) | Memory: 42.2 MB (30.97%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -6,12 +6,11 @@ class Solution {
         dp[0] = 1;
         dp[1] = 1;
 
-        for (int i = 2; i <= n; i++) {
-
-            for (int j = 1; j <= i; j++) {
-
+        for(int i = 2; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
                 dp[i] += dp[j - 1] * dp[i - j];
             }
+            
         }
 
         return dp[n];
