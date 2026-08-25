97c67abf0ada6f18cc9773c177805bcb0b18850e
Kunal0108 <kunalgurnani0108@gmail.com>
2026-08-25 14:14:20 -0300

    Time: 128 ms (87.23%) | Memory: 43.1 MB (43.60%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        
+
+        int m = board.length;
+        int n = board[0].length;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == word.charAt(0)) {
+                    if(dfs(board, word, i, j, 0)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean dfs(char[][] board, String word, int row, int column, int index) {
+
+        if(index == word.length()) {
+            return true;
+        }
+
+        if(row < 0 || row >= board.length ||
+            column < 0 || column >= board[0].length) {
+                return false;
+        }
+
+        if(board[row][column] != word.charAt(index)) {
+            return false;
+        }
+
+        char temp = board[row][column];
+        board[row][column] = '#';
+
+        boolean found = dfs(board, word, row - 1, column, index + 1) ||
+                        dfs(board, word, row + 1, column, index + 1) ||
+                        dfs(board, word, row, column - 1, index + 1) ||
+                        dfs(board, word, row, column + 1, index + 1);
+
+        board[row][column] = temp;
+
+        return found;
+
+    }
+}
